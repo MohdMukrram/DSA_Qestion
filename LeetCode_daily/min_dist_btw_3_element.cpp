02620d84c9e5b82ec624849e85d2df48fd441f1f
Mohd Mukrram <Mukrrammalik9528@gmail.com>
2026-04-10 15:05:07 +0530

    10 apr Daily question

---
+++ LeetCode_daily/min_dist_btw_3_element.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int minimumDistance(vector<int>& nums) {
+        int n = nums.size();
+        int dist = INT_MAX;
+
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (nums[i] == nums[j] && nums[j] == nums[k]) {
+                        int d = abs(i - j) + abs(j - k) + abs(k - i);
+                        dist = min(dist, d);
+                    }
+                }
+            }
+        }
+
+        return dist == INT_MAX ? -1 : dist;
+    }
